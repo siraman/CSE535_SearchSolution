cdd7df59b8c07a88af1818420038bc79e7a82e32
kiranprabhakar135 <kiranprabhakar135@gmail.com>
2018-11-26 15:24:26 -0500

    Integrating the changes:
    Need to still beautify the search results page
    Need to change the input in search results page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,30 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-
+import {NgxPaginationModule} from 'ngx-pagination';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeroesComponent} from './component/heroes/heroes.component';
 import {TweetComponent} from './component/tweet/tweet.component';
 import {QueryComponent} from './component/query/query.component';
 import {HashtagComponent} from './component/hashtags/hashtag.component';
+import {SearchComponent} from './component/search/search.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
     TweetComponent,
     QueryComponent,
-    HashtagComponent
+    HashtagComponent,
+    SearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/search/search.component.spec.ts
+++ src/app/component/search/search.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeroesComponent } from './heroes.component';
+import { SearchComponent } from './search.component';
 
-describe('HeroesComponent', () => {
-  let component: HeroesComponent;
-  let fixture: ComponentFixture<HeroesComponent>;
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroesComponent ]
+      declarations: [ SearchComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeroesComponent);
+    fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
