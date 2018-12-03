c2ca4fce6f7ef2adcf8a890861cb714da455fc1c
kiranprabhakar135 <kiranprabhakar135@gmail.com>
2018-12-03 09:04:26 -0500

    filter query changes, pagination changes etc

--- src/app/component/search/search.component.ts
+++ src/app/component/search/search.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FilterInputModel} from '../../query/filters/filter-input-model';
+import {forEach} from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-search',
@@ -11,13 +12,16 @@ export class SearchComponent implements OnInit {
 
   query: string;
   filterQuery: FilterInputModel[];
-
   captureQueryChangeEvent(event) {
     this.query = event;
   }
 
-  performFilterSearch(event) {
-    this.filterQuery = event;
+  performFilterSearch(event: FilterInputModel[]) {
+    const newFilterModel: FilterInputModel[] = [];
+    event.forEach(value => {
+      newFilterModel.push(value);
+    });
+    this.filterQuery = newFilterModel;
   }
 
   constructor(private activatedRoute: ActivatedRoute) {
